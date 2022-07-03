5ff8a10362c0d5dd37dc3de73c00a0400d4aaed1
YouhuaLi <youhua.li@demonware.net>
2022-07-03 15:16:46 -0700

    bug fix for packing_rectangle.py

--- rectangle_packing/packing_rectangle.py
+++ rectangle_packing/packing_rectangle.py
@@ -17,74 +17,57 @@ def adjust_boxes(i):
     #Algorithm step a6, Fig. 6. pefect fit on width.
     elif used_box[0] == Fraction(1, i+1):
         new_edge = used_box[1] - Fraction(1, i)
-        if new_edge < Fraction(1, total_rectangles + 1):
-            return True #new box is too small. skip adding.
-        if new_edge < used_box[0]:
-            new_box = (new_edge, used_box[0])
-        else:
-            new_box = (used_box[0], new_edge)
-        boxes.add(new_box) #new box of P_i in fig. 6
-        #current_box_index -= 1 #should always be successful beacuse we just add a smaller box before this step.
+        if new_edge >= Fraction(1, min_box_width_denominator + 1):
+            if new_edge < used_box[0]:
+                new_box = (new_edge, used_box[0])
+            else:
+                new_box = (used_box[0], new_edge)
+            boxes.add(new_box) #new box of P_i in fig. 6
     #Algorithm step a13. pefect fit on length.
     elif used_box[0] == Fraction(1, i):
         new_edge = used_box[1] - Fraction(1, i+1)
-        if new_edge < Fraction(1, total_rectangles + 1):
-            return True #new box is too small. skip adding.
-        if new_edge < used_box[0]:
-            new_box = (new_edge, used_box[0])
-        else:
-            new_box = (used_box[0], new_edge)
-        boxes.add(new_box)
-        #current_box_index -= 1 #should always be successful beacuse we just add a smaller box before this step.
+        if new_edge >= Fraction(1, min_box_width_denominator + 1):
+            if new_edge < used_box[0]:
+                new_box = (new_edge, used_box[0])
+            else:
+                new_box = (used_box[0], new_edge)
+            boxes.add(new_box)
     #Algorithm step a17. Fig. 7
     elif used_box[0] > Fraction(1, i):
         new_edge = used_box[0] - Fraction(1, i)
-        if new_edge < Fraction(1, total_rectangles + 1): 
-            return True #new box is too small. skip adding.
-        if new_edge < Fraction(1, i+1):
-            new_box = (new_edge, Fraction(1, i+1))
-        else:
-            new_box = (Fraction(1, i+1), new_edge)
-        #print("adding new box1: ", new_box)
-        boxes.add(new_box) #new box of C_i in fig. 7
+        if new_edge >= Fraction(1, min_box_width_denominator + 1): 
+            if new_edge < Fraction(1, i+1):
+                new_box = (new_edge, Fraction(1, i+1))
+            else:
+                new_box = (Fraction(1, i+1), new_edge)
+            boxes.add(new_box) #new box of C_i in fig. 7
+        
         new_edge = used_box[1] - Fraction(1, i + 1)
-        if new_edge < Fraction(1, total_rectangles + 1): 
-            return True #new box is too small. skip adding.
-        if used_box[0] < new_edge:
-            new_box = (used_box[0] , new_edge)
-        else:
-            new_box = (new_edge, used_box[0])
-        #print("adding new box2: ", new_box)
-        boxes.add(new_box) #new box of D_i in fig. 7
-        #current_box_index -= 1 #should always be successful beacuse we just add two smaller boxes before this step.
+        if new_edge >= Fraction(1, min_box_width_denominator + 1): 
+            if used_box[0] < new_edge:
+                new_box = (used_box[0] , new_edge)
+            else:
+                new_box = (new_edge, used_box[0])
+            boxes.add(new_box) #new box of D_i in fig. 7
     #Algorithm step a21. Fig. 8
     else:
         new_edge = used_box[0] - Fraction(1, i+1)
-        if new_edge < Fraction(1, total_rectangles + 1): 
-            return True #new box is too small. skip adding.
-        if new_edge < Fraction(1, i):
-            new_box = (new_edge, Fraction(1, i))
-        else:
-            new_box = (Fraction(1, i), new_edge)
-        #print("adding new box: ", new_box)
-        boxes.add(new_box) #new box of C_i in fig. 8
+        if new_edge >= Fraction(1, min_box_width_denominator + 1): 
+            if new_edge < Fraction(1, i):
+                new_box = (new_edge, Fraction(1, i))
+            else:
+                new_box = (Fraction(1, i), new_edge)
+            boxes.add(new_box) #new box of C_i in fig. 8
+        
         new_edge = used_box[1] - Fraction(1, i)
-        if new_edge < Fraction(1, total_rectangles + 1): 
-            return True #new box is too small. skip adding.
-        if used_box[0] < new_edge:
-            new_box = (used_box[0] , new_edge)
-        else:
-            new_box = (new_edge, used_box[0])
-        #print("adding new box: ", new_box)
-        boxes.add(new_box) #new box of D_i in fig. 8
-        #current_box_index -= 1 #should always be successful beacuse we just add two smaller boxes before this step.
+        if new_edge >= Fraction(1, min_box_width_denominator + 1): 
+            if used_box[0] < new_edge:
+                new_box = (used_box[0] , new_edge)
+            else:
+                new_box = (new_edge, used_box[0])
+            boxes.add(new_box) #new box of D_i in fig. 8
     
-    #print("boxes list BEFORE delete is:", boxes)
-    #print("the box", boxes[current_box_index],  " is used by rectangle (1/%d, 1/%d) and being deleted." \
-    #     % (i+1, i))
     boxes.remove(used_box)
-    #print("boxes list AFTER delete is:", boxes)
-    #print("\n")
 
     return True
 
@@ -123,7 +106,8 @@ boxes = SortedList()
 boxes.add((Fraction(1, 2),  Fraction(1, 1)))
 
 current_box_index = 0
-total_rectangles = 10 ** 3
+min_box_width_denominator = 10 ** 9
+total_rectangles = 10 ** 6
 
 
 for i in range(2, total_rectangles + 1):
@@ -132,10 +116,18 @@ for i in range(2, total_rectangles + 1):
         break
 
 if packing_result:
-    print("success to fill till rectagle (1 / %d, 1 / %d)" % (i+1, i))
-    print("remaining boxes size (width > 1 / %d) is: %d" % (total_rectangles + 1, len(boxes)))
+    print("successful to pack rectagle (1 / %d, 1 / %d)" % (i + 1, i))
+    print("remaining boxes size (width > 1 / %d) is: %d" % (min_box_width_denominator + 1, len(boxes)))
     print("the lagest 5 remaining boxes are:")
     for box in boxes[-5:]:
-        print(box)
+        print(box, (box[0].numerator / box[0].denominator), " X ", (box[1].numerator / box[1].denominator))
 else:
-    print("failed")
+    print("failed to pack rectagle (1 / %d, 1 / %d)" % (i+1, i))
+
+# size = 0
+# for j in range(2, i + 1 ):
+#     size += (1 / (j + 1)) * (1 / j)
+# for box in boxes:
+#     box_size = box[0] * box[1]
+#     size += box_size.numerator / box_size.denominator
+# print(size)
